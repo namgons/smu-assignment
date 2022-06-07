1869dd178e655c0990489eef304ad6d0f6260ac9
namgon <loginonlyyy@gmail.com>
2022-06-07 15:22:11 +0900

    created UserFollowing Model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from core import models as core_models
 
 
 class User(AbstractUser):
 
     pass
+
+
+class UserFollowing(core_models.TimeStampedModel):
+    user_id = models.ForeignKey(User, related_name="follwing")
+
+    following_user_id = models.ForeignKey(User, related_name="follower")
