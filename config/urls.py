74ff9a878b71e9a95a194a8fef79033a08d0f1c3
namgon <loginonlyyy@gmail.com>
2022-06-07 15:55:27 +0900

    created urls files

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(("core/"), include("core.urls", namespace="core")),
+    path(("movies/"), include("movies.urls", namespace="movies")),
+    path(("reviews/"), include("reviews.urls", namespace="reviews")),
+    path(("users/"), include("users.urls", namespace="users")),
+    path("admin/", admin.site.urls),
 ]
