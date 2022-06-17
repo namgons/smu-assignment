a801eaf3a1c36b596f499b309ec79cf751ef8cfd
namgon <loginonlyyy@gmail.com>
2022-06-17 22:58:02 +0900

    styled contents view

--- contents/urls.py
+++ contents/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "contents"
 
 urlpatterns = [
-    path("", views.ContentsView.as_view(), name="contents"),
+    path("movies/", views.MoviesView.as_view(), name="movies"),
+    path("tvs/", views.TvsView.as_view(), name="tvs"),
     path("<int:pk>/", views.SingleContentView.as_view(), name="single_content"),
     path(
         "<int:pk>/reviews/create/",
--- contents/views.py
+++ contents/views.py
@@ -3,13 +3,28 @@ from django.views.generic import ListView, View
 from . import models as content_models
 
 
-class ContentsView(ListView):
+class MoviesView(ListView):
 
     model = content_models.Content
-    paginate_by = 10
+    paginate_by = 40
     context_object_name = "contents"
-    template_name = "contents.html"
-    
+    template_name = "contents/contents.html"
+
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(media_type=content_models.Content.TYPE_MOVIE)
+        )
+
+
+class TvsView(ListView):
+
+    model = content_models.Content
+    paginate_by = 40
+    context_object_name = "contents"
+    template_name = "contents/contents.html"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(media_type=content_models.Content.TYPE_TV)
 
 
 class SingleContentView(View):
