5c3901a7a0a1993e44e86ec9c0c787f2fd86a887
namgon <loginonlyyy@gmail.com>
2022-06-18 23:24:57 +0900

    created form-template mixins

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from . import models
+from contents import models as content_models
 
 
 class ReviewCreateView(CreateView):
 
     model = models.Review
     fields = ("comment",)
+    template_name = "reviews/create.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs["pk"]
+        context["content"] = content_models.Content.objects.get(pk=pk)
+        return context
