650fbc5f40e53314ac0da64fa5272e61694809a4
namgon <loginonlyyy@gmail.com>
2022-06-19 16:08:19 +0900

    leave comments

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import CreateView
 from . import models
 from contents import models as content_models
@@ -15,3 +16,11 @@ class ReviewCreateView(CreateView):
         pk = self.kwargs["pk"]
         context["content"] = content_models.Content.objects.get(pk=pk)
         return context
+
+    def form_valid(self, form):
+        comment = form.cleaned_data["comment"]
+        user = self.request.user
+        pk = self.kwargs["pk"]
+        content = content_models.Content.objects.get(pk=pk)
+        models.Review.objects.create(comment=comment, user=user, content=content)
+        return redirect(reverse("contents:single_content", kwargs={"pk": pk}))
